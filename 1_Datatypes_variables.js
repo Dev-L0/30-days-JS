bbe01f61510b69d153b3daada754d7ddd3ce1d52
L. <64107059+Dev-L0@users.noreply.github.com>
2024-07-13 16:50:34 +0000

    ..

---
+++ 1_Datatypes_variables.js
@@ -0,0 +1,40 @@
+//Variable Declaration
+var number = 7;
+console.log(number);
+
+let string = "Hello world!";
+console.log(string);
+
+//Constant Declaration
+const bool = true;
+console.log(bool);
+
+//Datatypes
+let num1 = 123;
+let str1 = "Chai";
+let bool1 = false;
+let arr1= [];
+let obj1 = {};
+
+console.table([[num1, typeof(num1)],[str1,typeof(str1)],[bool1, typeof(bool1)], [arr1, typeof(arr1)], [obj1, typeof(obj1)]]);
+
+//Reassigning variables
+//let
+let b = 123;
+console.log(b);
+b = 321;
+console.log(b);
+
+//Understanding const
+const pi = 3.14;
+console.log(pi);
+pi = 123;
+console.log(pi); //TypeError: Assignment to constant variable.
+
+
+
+
+
+
+
+
