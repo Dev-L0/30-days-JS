5863864a59dae190e60e6eb7868d3765625d9eaa
L. <64107059+Dev-L0@users.noreply.github.com>
2024-07-14 12:19:30 +0000

    ..

---
+++ 2_Operators.js
@@ -0,0 +1,69 @@
+//Activity 1 : Arithemetic Operators
+
+//Task 1: Add two numbers
+let num1 = 7;
+let num2 = 6;
+console.log(num1 + num2);
+
+//Task 2: Subttract two numbers
+console.log(num1 - num2);
+
+//Task 3: Multiply two numbers
+console.log(num1 * num2);
+
+//Task 4: Divide two numbers
+console.log(num1 / num2);
+
+//Task 5: Remainder
+console.log(num1 % num2);
+
+
+//Activity 2: Assignment Operators
+//Task 6:  +=
+let num3 = 76;
+console.log(num3 += 1);
+//Task 7:  -=
+let num4 = 7;
+console.log(num4 -= 1);
+
+//Activity 3: Comparison Operators
+
+let a = 96;
+let b = 97;
+let c = 96;
+
+//Task 8: Compare > , <
+console.log(a > b);
+
+//Task 8: >= , <=
+console.log(a >= c);
+console.log(a <= b);
+
+//Task 9: == , ===
+console.log(a == b);
+console.log(a === c);
+
+
+//Activity 4: Logical Operators
+//Task 11: && Operator
+let number = 3 > 4 && 3 < 2;
+console.log(number);
+let number2 = 7 > 6 && 6 < 7;
+console.log(number2);
+
+//Task 12: || Operator
+let number3 = 3 > 4 || 2 < 3;
+console.log(number3);
+
+//Task 13: ! Operator
+let bool = !false;
+let number4 = !7;
+console.log(bool);
+console.log(number4);
+
+//Activity 5: Ternary Opertaor
+//Task 14: 
+
+console.log(5 > 2 ? "5 is greater than 2" : "5 is less than 2");
+
+
